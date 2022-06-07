a9f5d53d312ffe43a4eb4332c2c57455d39a0e55
BMafra <bruna_a_mafra@estudante.sesisenai.org.br>
2022-06-07 14:32:43 -0300

    commit

---
+++ src/Rio.java
@@ -0,0 +1,17 @@
+public class Rio {
+    int lado;
+    // 1 = direita
+    // 2 = esquerda
+
+    public Rio(int lado) {
+        this.lado = lado;
+    }
+
+    public int getLado() {
+        return lado;
+    }
+
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+}
